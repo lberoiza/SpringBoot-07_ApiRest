7bd0ea3838d54366926aa1001f5bdbb20f7ec797
Luis B <luis.beroiza@adesso.de>
2023-08-29 14:34:30 +0200

    Agrega el resto de Metodos CRUD de Clientes

--- src/main/java/com/lab/springboot/backend/apirest/controllers/ClientRestController.java
+++ src/main/java/com/lab/springboot/backend/apirest/controllers/ClientRestController.java
@@ -3,12 +3,11 @@ package com.lab.springboot.backend.apirest.controllers;
 import com.lab.springboot.backend.apirest.models.entity.Client;
 import com.lab.springboot.backend.apirest.models.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -23,8 +22,41 @@ public class ClientRestController {
   }
 
   @GetMapping("/list")
-  public List<Client> getClients(){
-    return clientService.findAll();
+  public List<Client> getClients() {
+    return this.clientService.findAll();
+  }
+
+  @GetMapping("/{id}")
+  public Client getClientDetails(@PathVariable Long id) {
+    return this.clientService.findById(id).orElse(null);
+  }
+
+  @PostMapping("")
+  @ResponseStatus(HttpStatus.CREATED)
+  public Client save(@RequestBody Client client) {
+    return this.clientService.saveOrUpdate(client);
+  }
+
+  @PutMapping("/{id}")
+  @ResponseStatus(HttpStatus.CREATED)
+  public Client update(@RequestBody Client client, @PathVariable Long id) {
+    Optional<Client> optionalClient = this.clientService.findById(id);
+    if (optionalClient.isEmpty()) {
+      return null;
+    }
+
+    Client currentClient = optionalClient.get();
+    currentClient.setName(client.getName());
+    currentClient.setSurname(client.getSurname());
+    currentClient.setImage(client.getImage());
+    currentClient.setEmail(client.getEmail());
+    return this.clientService.saveOrUpdate(currentClient);
+  }
+
+  @DeleteMapping("/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void deleteClient(@PathVariable Long id) {
+    this.clientService.deleteById(id);
   }
 
 
