ea3bfafc89b2679677276d62f41e08dac7826db1
Luis B <luis.beroiza@adesso.de>
2023-08-31 16:18:08 +0200

    Agrega Methodos para buscar por Id con Excepcion o Chequear si un Email existe

--- src/main/java/com/lab/springboot/backend/apirest/models/service/ClientService.java
+++ src/main/java/com/lab/springboot/backend/apirest/models/service/ClientService.java
@@ -2,7 +2,12 @@ package com.lab.springboot.backend.apirest.models.service;
 
 import com.lab.springboot.backend.apirest.models.entity.Client;
 import com.lab.springboot.backend.apirest.models.repository.ClientRepository;
+import com.lab.springboot.backend.apirest.utils.ApiServiceException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class ClientService extends BaseCRUDService<Client, ClientRepository>{
@@ -10,4 +15,22 @@ public class ClientService extends BaseCRUDService<Client, ClientRepository>{
   public boolean existsEmail(String email){
     return this.repository.existsByEmail(email);
   }
+
+  public Client findByIdOrThrowsException(Long id) throws ApiServiceException{
+    Optional<Client> clientOptional = this.repository.findById(id);
+    if (clientOptional.isEmpty()) {
+      String error = String.format("Client with id (%d) not found", id);
+      throw new ApiServiceException(error, HttpStatus.NOT_FOUND);
+    }
+    return clientOptional.get();
+  }
+
+  public boolean checkIfEmailExistByAnotherUser(Client client, String email){
+    Client clientWithEmail = this.repository.findByEmail(email);
+    if(clientWithEmail == null ) {
+      return false;
+    }
+    return !Objects.equals(client.getId(), clientWithEmail.getId());
+  }
+
 }
