e4fed9d707f4d35bf0004753e7c4b794d4d84820
Luis B <luis.beroiza@adesso.de>
2023-08-31 15:12:52 +0200

    Agrega manejo de excepciones en el Backend para Controlador de Clientes

--- src/main/java/com/lab/springboot/backend/apirest/controllers/ClientRestController.java
+++ src/main/java/com/lab/springboot/backend/apirest/controllers/ClientRestController.java
@@ -2,8 +2,11 @@ package com.lab.springboot.backend.apirest.controllers;
 
 import com.lab.springboot.backend.apirest.models.entity.Client;
 import com.lab.springboot.backend.apirest.models.service.ClientService;
+import com.lab.springboot.backend.apirest.utils.ApiResponse;
+import com.lab.springboot.backend.apirest.utils.ResponseEntityHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,41 +25,74 @@ public class ClientRestController {
   }
 
   @GetMapping("/list")
-  public List<Client> getClients() {
-    return this.clientService.findAll();
+  public ResponseEntity<ApiResponse<List<Client>>> getClients() {
+    return ResponseEntityHandler.handleApiResponse(() -> {
+      ApiResponse<List<Client>> response = new ApiResponse<>();
+      List<Client> clients = this.clientService.findAll();
+      response.addResponseData(clients);
+      if(clients.isEmpty()){
+        response.addWarning("There are no clients");
+      }
+      return response;
+    });
   }
 
   @GetMapping("/{id}")
-  public Client getClientDetails(@PathVariable Long id) {
-    return this.clientService.findById(id).orElse(null);
+  public ResponseEntity<ApiResponse<Client>> getClientDetails(@PathVariable Long id) {
+    return ResponseEntityHandler.handleApiResponse(() -> {
+      ApiResponse<Client> response = new ApiResponse<>();
+      Optional<Client> clientOptional = this.clientService.findById(id);
+      if(clientOptional.isEmpty()) {
+        response.addError(String.format("Client with id (%d) not found", id));
+        response.setHttpStatus(HttpStatus.NOT_FOUND);
+      } else {
+        response.addResponseData(clientOptional.get());
+      }
+      return response;
+    });
   }
 
   @PostMapping("")
-  @ResponseStatus(HttpStatus.CREATED)
-  public Client save(@RequestBody Client client) {
-    return this.clientService.saveOrUpdate(client);
+  public ResponseEntity<ApiResponse<Client>> save(@RequestBody Client client) {
+    return ResponseEntityHandler.handleApiResponse(() -> {
+      ApiResponse<Client> response = new ApiResponse<>();
+      response.addResponseData(this.clientService.saveOrUpdate(client));
+      return response;
+    });
   }
 
   @PutMapping("/{id}")
-  @ResponseStatus(HttpStatus.CREATED)
-  public Client update(@RequestBody Client client, @PathVariable Long id) {
-    Optional<Client> optionalClient = this.clientService.findById(id);
-    if (optionalClient.isEmpty()) {
-      return null;
-    }
+  public ResponseEntity<ApiResponse<Client>>  update(@RequestBody Client client, @PathVariable Long id) {
 
-    Client currentClient = optionalClient.get();
-    currentClient.setName(client.getName());
-    currentClient.setSurname(client.getSurname());
-    currentClient.setImage(client.getImage());
-    currentClient.setEmail(client.getEmail());
-    return this.clientService.saveOrUpdate(currentClient);
+    return ResponseEntityHandler.handleApiResponse(() -> {
+      ApiResponse<Client> response = new ApiResponse<>();
+
+      Optional<Client> optionalClient = this.clientService.findById(id);
+      if (optionalClient.isEmpty()) {
+        response.addError(String.format("Client with id (%d) not found", id));
+        response.setHttpStatus(HttpStatus.NOT_FOUND);
+        return response;
+      }
+
+      Client currentClient = optionalClient.get();
+      currentClient.setName(client.getName());
+      currentClient.setSurname(client.getSurname());
+      currentClient.setImage(client.getImage());
+      currentClient.setEmail(client.getEmail());
+
+      response.addResponseData(this.clientService.saveOrUpdate(currentClient));
+      return response;
+    });
   }
 
   @DeleteMapping("/{id}")
   @ResponseStatus(HttpStatus.NO_CONTENT)
-  public void deleteClient(@PathVariable Long id) {
-    this.clientService.deleteById(id);
+  public ResponseEntity<ApiResponse<Client>> deleteClient(@PathVariable Long id) {
+    return ResponseEntityHandler.handleApiResponse(() -> {
+      this.clientService.deleteById(id);
+      return null;
+    });
+
   }
 
 
