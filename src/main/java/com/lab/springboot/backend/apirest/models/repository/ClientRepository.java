0447c47969f7fcb5ccf247e3e1b44d8fb10c9045
Luis B <luis.beroiza@adesso.de>
2023-08-31 16:17:08 +0200

    Busca cliente por email

--- src/main/java/com/lab/springboot/backend/apirest/models/repository/ClientRepository.java
+++ src/main/java/com/lab/springboot/backend/apirest/models/repository/ClientRepository.java
@@ -4,6 +4,10 @@ package com.lab.springboot.backend.apirest.models.repository;
 import com.lab.springboot.backend.apirest.models.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClientRepository extends JpaRepository<Client, Long> {
   boolean existsByEmail(String Email);
+
+  Client findByEmail(String Email);
 }
